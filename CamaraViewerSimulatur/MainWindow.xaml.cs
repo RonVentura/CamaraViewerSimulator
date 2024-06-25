51375e0fdf46cb3e79992722f3cabace0bb86714
Ronve <ronventura17@gmail.com>
2024-06-25 15:02:10 +0300

    Basic Gui Creation

--- CamaraViewerSimulatur/MainWindow.xaml.cs
+++ CamaraViewerSimulatur/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,10 +16,24 @@ namespace CamaraViewerSimulatur
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
     {
+        private BitmapImage chosenImage;
+        private WriteableBitmap loadedImage;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadImage_click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                chosenImage = new BitmapImage(new Uri(openFileDialog.FileName));
+                loadedImage = new WriteableBitmap(chosenImage);
+                ImageViewer.Source = loadedImage;
+            }
         }
     }
 }
