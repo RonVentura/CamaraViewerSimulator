9ec302d2fb5bd65ed5c0f33d60422828a87548b0
Ronve <Ronve@RONPC>
2024-06-25 14:06:02 +0300

    Add project files.

---
+++ CamaraViewerSimulatur/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CamaraViewerSimulatur
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
