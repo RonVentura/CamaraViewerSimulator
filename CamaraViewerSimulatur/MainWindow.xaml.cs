ad54450f3d98d978d54273360d0982708357888a
Ronve <ronventura17@gmail.com>
2024-06-26 11:04:18 +0300

    - Dynamically change histogram - Adjusting presentation - Code fixes

--- CamaraViewerSimulatur/MainWindow.xaml.cs
+++ CamaraViewerSimulatur/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace CamaraViewerSimulatur
     {
         private BitmapImage chosenImage;
         private WriteableBitmap loadedImage;
+        private int width;
+        private int height;
+        private int pixels_to_byte;
+        private byte[] pixels;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,10 +32,43 @@ namespace CamaraViewerSimulatur
         private void LoadImage_click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            Slider.Value = 10;
             if (openFileDialog.ShowDialog() == true)
             {
                 chosenImage = new BitmapImage(new Uri(openFileDialog.FileName));
                 loadedImage = new WriteableBitmap(chosenImage);
+                ImageViewer.Source = loadedImage;
+                width = loadedImage.PixelWidth;
+                height = loadedImage.PixelHeight;
+                pixels_to_byte = width * ((loadedImage.Format.BitsPerPixel + 7) / 8); // round bytes to fit pixels number
+                pixels = new byte[height * pixels_to_byte];
+                loadedImage.CopyPixels(pixels, pixels_to_byte, 0);
+                calc_hist();
+            }
+        }
+
+        private void SliderChange(object sender, RoutedPropertyChangedEventArgs<double> e) 
+        {
+            double exp = (Slider.Value)/ 10;
+            ChangeExposure(exp);
+        }
+
+        private void ChangeExposure(double exp)
+        {   
+            if (ImageViewer.Source == null) 
+            {
+                return;
+            }
+
+            if (loadedImage.Format.BitsPerPixel == 8) // Assuming image is 8-bit grayscale
+            {
+                byte[] new_pixels = new byte[height * pixels_to_byte];
+                for (int i = 0; i < pixels.Length; i++) // change pixels vals
+                {
+                    new_pixels[i] = (byte)Math.Clamp((int)(pixels[i]*exp),0,255);
+                }
+
+                loadedImage.WritePixels(new Int32Rect(0, 0, width, height), new_pixels, pixels_to_byte, 0);
                 ImageViewer.Source = loadedImage;
                 calc_hist();
             }
@@ -39,17 +76,15 @@ namespace CamaraViewerSimulatur
 
         private void calc_hist()
         {
+            HistCanvas.Children.Clear();
             int[] hist = new int[256];
-            int width = loadedImage.PixelWidth;
-            int height = loadedImage.PixelHeight;
             if (loadedImage.Format.BitsPerPixel == 8) // Assuming image is 8-bit grayscale
             {
-                int pixels_to_byte = width * ((loadedImage.Format.BitsPerPixel + 7) / 8); // round bytes to fit pixels number
-                byte[] pixels = new byte[height * pixels_to_byte];
-                loadedImage.CopyPixels(pixels, pixels_to_byte, 0);
-                for (int i = 0; i < height; i++) // create histogram
+                byte[] curr_pixels = new byte[height * pixels_to_byte];
+                loadedImage.CopyPixels(curr_pixels, pixels_to_byte, 0);
+                for (int i = 0; i < curr_pixels.Length; i++) // create histogram
                 {
-                    hist[pixels[i]]++; 
+                    hist[curr_pixels[i]]++; 
                 }
             }
 
@@ -65,7 +100,7 @@ namespace CamaraViewerSimulatur
 
             for (int j = 0; j < hist.Length; j++) // Draw hist
             {
-                int x = (int)((j / (double)255) * HistCanvas.Width);
+                int x = (int)((j * HistCanvas.Width / (double)255));
                 Line line = new Line
                 {   
                     X1 = x,
@@ -73,7 +108,7 @@ namespace CamaraViewerSimulatur
                     X2 = x,
                     Y2 = HistCanvas.Height - hist[j],
                     Stroke = Brushes.MediumPurple,
-                    StrokeThickness = 3
+                    StrokeThickness = HistCanvas.Width/256
                 };
                 HistCanvas.Children.Add(line);
             }
